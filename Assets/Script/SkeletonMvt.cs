12cb33ab3d99df834131ee2b4cc008c6d4af2881
MoOaAaa <71814380+MoOaAaa@users.noreply.github.com>
2022-02-24 16:27:05 -0500

    Skeleton follow player

--- Assets/Script/SkeletonMvt.cs
+++ Assets/Script/SkeletonMvt.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class SkeletonMvt : MonoBehaviour
 {
     Animator animator;
+    Transform playerHead;
+    public float rotateSpeed = 1f;
+    public float movementSpeed = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        playerHead = GameManager.instance.PlayerHead;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float movement = 0.5f;
-        animator.SetFloat("Speed",movement);
-        transform.position += Time.deltaTime * movement * 4 * Vector3.forward;
+        animator.SetFloat("Speed",movementSpeed);
+        Vector3 headPos = new Vector3(playerHead.position.x, transform.position.y, playerHead.position.z);
+        Vector3 headDirection = headPos - transform.position;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(headDirection, Vector3.up),Time.deltaTime * rotateSpeed);
+        transform.position += Time.deltaTime * movementSpeed * 2 * transform.forward;
+
+        Debug.Log(GameManager.instance.PlayerHead.position);
     }
 }
