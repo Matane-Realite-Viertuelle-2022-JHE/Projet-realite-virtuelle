7d619f9bacfe87bf9055aaeb1014f64b76fa0beb
MoOaAaa <71814380+MoOaAaa@users.noreply.github.com>
2022-03-17 16:11:51 -0400

    test collision

--- Assets/Script/CollisionDetection.cs
+++ Assets/Script/CollisionDetection.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class CollionDetection : MonoBehaviour
+public class CollisionDetection : MonoBehaviour
 {
+    XRGrabInteractable xrGrab;
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "enemy")
+
+        if (other.tag == "enemy")
         {
+            xrGrab = GetComponent<XRGrabInteractable>();
+            Debug.Log(xrGrab.isSelected);
             Debug.Log(other.name);
             other.GetComponent<SkeletonBehavior>().TakeDamage(100);
         }
